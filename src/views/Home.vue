1b2726350391162da9c70d76d57847e7b53e7bed
CureauRemi <remicureau35@gmail.com>
2021-03-26 14:19:33 +0100

    Adding Temperature fetch

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,63 @@
 <template>
-  <div class="home">
-    <h1>This is a home page</h1>
-  </div>
+    <v-container>
+    <v-row no-gutters>
+      <v-col
+        cols="12"
+        sm="4"
+      >
+        <v-card
+          class="pa-2"
+          elevation="0"
+        >
+          <v-card-title>Temperature Ambiante</v-card-title>
+          <v-card-title>{{ temperature}}°C</v-card-title>
+        </v-card>
+      </v-col>
+      <v-col
+        cols="12"
+        sm="4"
+      >
+        <v-card
+          class="pa-2"
+          elevation="0"
+        >
+          
+        </v-card>
+      </v-col>
+      <v-col
+        cols="12"
+        sm="4"
+      >
+        <v-card
+          class="pa-2"
+          elevation="0"
+        >
+          
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+import Service from '../services/temperature-service';
 export default {
   name: 'HomePage',
   components: {
+  },
+  data () {
+    return {
+      temperature: 0,
+    }
+  },
+  methods: {
+    getTemperature () {
+       Service.get('api/temperature').then( response => {
+        this.temperature = response.data.result
+      }).catch( error => {
+        console.error('error', error)
+      })
+    }
   }
 }
 </script>
