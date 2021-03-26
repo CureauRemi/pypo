3610e4b22164a849407c059a944cac43627ae2c6
CureauRemi <remicureau35@gmail.com>
2021-03-26 14:32:17 +0100

    Adding sync to the temperature

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,13 +4,17 @@
       <v-col
         cols="12"
         sm="4"
+        
       >
         <v-card
-          class="pa-2"
+          class="pa-2 justify-center"
           elevation="0"
         >
           <v-card-title>Temperature Ambiante</v-card-title>
           <v-card-title>{{ temperature}}°C</v-card-title>
+          <v-btn @click="getTemperature()" icon>
+            <v-icon>fas fa-sync-alt</v-icon>
+          </v-btn>
         </v-card>
       </v-col>
       <v-col
